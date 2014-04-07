12727640548af34fe5455815e755d3f85df1c32a
Firdaus Adib <i@firdaus.my>
2014-04-08 07:33:27 +0800

    Update HelloServer.java

--- HelloServer.java
+++ HelloServer.java
@@ -1,13 +1,8 @@
-
 import java.rmi.*;
 
 public class HelloServer
 {
 
-/**
-  * Server program for the "Hello, world!" example.
-  * @param argv The command line arguments which are ignored.
-  */
 public static void main (String[] argv) {
    try {
      Naming.rebind ("Hello", new Hello ("Hello, world!"));
