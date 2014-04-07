a78d40285070b029dc376feeffb288b8f6a6c71f
Firdaus Adib <i@firdaus.my>
2014-04-08 07:25:33 +0800

    Push, push, push

---
+++ HelloServer.java
@@ -0,0 +1,20 @@
+
+import java.rmi.*;
+
+public class HelloServer
+{
+
+/**
+  * Server program for the "Hello, world!" example.
+  * @param argv The command line arguments which are ignored.
+  */
+public static void main (String[] argv) {
+   try {
+     Naming.rebind ("Hello", new Hello ("Hello, world!"));
+     System.out.println ("Hello Server is ready.");
+   } catch (Exception e) {
+     System.out.println ("Hello Server failed: " + e);
+   }
+}
+
+}
